768ef5cc811003a891e1b3b5ae6cf6c88966e3cc
Erick Penado <penado188@gmail.com>
2022-11-22 16:22:32 -0600

    Primeros cambios

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const btnMenu = document.getElementById('btn_menu');
+const ltMenu = document.getElementById('lt-nav');
+const btnClose = document.getElementById('btn_close');
+const bgMenu = document.getElementById('back_menu');
+document.getElementById('back_menu').addEventListener('click', ocultar);
+
+function ocultar(){
+    ltMenu.classList.remove('nav-toggle')
+    bgMenu.style.display = "none";
+}
+
+btnMenu.addEventListener('click', () =>{
+    ltMenu.classList.toggle('nav-toggle')
+    bgMenu.style.display = "block";
+});
+
+btnClose.addEventListener('click', ocultar);
