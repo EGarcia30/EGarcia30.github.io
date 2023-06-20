80884af96cc9936c92b9226e61ec428c798e5a10
Erick Penado <penado188@gmail.com>
2023-06-20 02:24:14 -0600

    deploy sh

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/egarcia30.github.io/",
-  build: {
-    assetsInclude: ["**/*.js"],
-    rollupOptions: {
-      output: {
-        mimeTypes: {
-          ".js": "application/javascript"
-        }
-      }
-    }
-  }
+  base: "/egarcia30.github.io/"
 })
