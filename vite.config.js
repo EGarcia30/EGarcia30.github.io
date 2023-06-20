c0e7d75db329eb10198eb068175380c1b0f2d380
Erick Penado <penado188@gmail.com>
2023-06-20 02:20:36 -0600

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/egarcia30.github.io/",
   build: {
     assetsInclude: ["**/*.js"],
     rollupOptions: {
